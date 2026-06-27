270ba37e1cb4414a60fcbb603171aba2bad2a16e
nayanranjanrath <nayanranjanrath5@gmail.com>
2026-06-27 20:53:16 +0530

    used react routes

--- moviesite/src/App.jsx
+++ moviesite/src/App.jsx
@@ -2,14 +2,17 @@
 import Moviecard from './component/moviecart'
 import Home from './pages/home'
 import './App.css'
-
+import { Routes, Route } from 'react-router-dom'
 function App() {
  
 
   return (
-    <>
-    <Home></Home>
-    </>
+    <main className="maincontent">
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        
+      </Routes>
+    </main>
   )
 }
 function Text({ children }) {
